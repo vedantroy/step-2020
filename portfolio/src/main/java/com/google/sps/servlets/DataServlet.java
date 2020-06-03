b4c02f28aa8a29912aa641eecc78eaac897a8954
Vedant Roy <vroy101@gmail.com>
2020-06-03 17:58:12 -0400

    Use datastore instead of in-memory ArrayList

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,12 +23,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.gson.Gson;
 
 /** Servlet that returns some example content.*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
   private final ArrayList<String> comments = new ArrayList<>();
 
   public DataServlet() {
@@ -40,13 +46,21 @@ public class DataServlet extends HttpServlet {
     String comment = req.getParameter("comment");
     if (comment != null && !comment.equals("")) {
       // do nothing if the comment field is empty
-      comments.add(comment);
+      Entity commentEntity = new Entity("comment");
+      commentEntity.setProperty("value", comment);
+      datastore.put(commentEntity);
     }
     resp.sendRedirect("/index.html");
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("comment");
+    PreparedQuery results = datastore.prepare(query);
+    ArrayList<String> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      comments.add((String)entity.getProperty("value"));
+    }
     response.setContentType("application/json;");
     response.getWriter().println((new Gson()).toJson(comments));
   }
