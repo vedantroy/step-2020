37f7f56495be9718a132c5b4e4d0d8c87844bbff
Vedant Roy <vroy101@gmail.com>
2020-06-05 01:39:11 -0400

    Implement scheduling for non-optional attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,46 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.sps.Event;
+import com.google.sps.TimeRange;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Comparator<Event> comparator = new Comparator<Event>() {
+      @Override
+      public int compare(final Event e1, final Event e2) {
+        return TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen());
+      }
+    };
+    List<Event> l = new ArrayList(events);
+    Collections.sort(l, comparator);
+    Collection<TimeRange> ranges = new ArrayList<TimeRange>();
+    int start = TimeRange.START_OF_DAY;
+    for (Event e : l) {
+      Set<String> attendees = e.getAttendees();
+      Set<String> copy = new HashSet<>(attendees);
+      copy.retainAll(request.getAttendees());
+      if (copy.size() > 0) {
+        int end = e.getWhen().start();
+        if (end - start >= request.getDuration()) {
+          ranges.add(TimeRange.fromStartEnd(start, end, false));
+        }
+        int potentialNewStart = e.getWhen().end();
+        if (potentialNewStart > start) {
+          start = potentialNewStart;
+        }
+      }     
+    }
+    if (TimeRange.END_OF_DAY - start >= request.getDuration()) {
+      ranges.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+    }
+    return ranges;
   }
 }
