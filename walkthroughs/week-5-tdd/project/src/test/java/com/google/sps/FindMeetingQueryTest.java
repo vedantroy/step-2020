1fe8e559e4a0ac24e4664a3de96c3270706ede2a
Vedant Roy <vroy101@gmail.com>
2020-06-15 21:36:06 -0400

    Remove unapplicable comment

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -126,13 +126,6 @@ public final class FindMeetingQueryTest {
 
   @Test
   public void ConsiderOptionalAttendee() {
-    // Have each person have different events. We should see two options because each person has
-    // split the restricted times.
-    //
-    // Events  :       |--A--|     |--B--|
-    // Day     : |-----------------------------|
-    // Options : |--1--|     |--2--|     |--3--|
-
     Collection<Event> events = Arrays.asList(
         new Event("Event 1", TimeRange.fromStartDuration(TIME_0800AM, DURATION_30_MINUTES),
             Arrays.asList(PERSON_A)),
